0eb0499001c365564719d6b27318b5c9b1484a87
Vinicius-Muller <viniciusmul98@gmail.com>
2025-03-02 13:14:16 -0300

    v0.2

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,18 +1,5 @@
-const express = require("express");
-const cors = require("cors");
-
 const Service = require("./ws/index.js");
-
 
 const wss = new Service();
 wss.setWs();
 wss.connection();
-
-const app = express();
-app.use(cors());
-
-app.get("/", (req, res) => {
-  res.send("Server is running on Vercel!");
-});
-
-export default app;
