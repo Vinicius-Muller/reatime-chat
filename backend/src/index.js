fc89ca3fa53a82dee85e79fabe06600333a90140
Vinicius-Muller <viniciusmul98@gmail.com>
2025-03-02 11:53:19 -0300

    integração OOP

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,5 @@
-const { WebSocketServer } = require("ws");
-const dotenv = require("dotenv");
+import Service from "./ws";
 
-dotenv.config();
-
-const wss = new WebSocketServer({ port: process.env.PORT || 8080 });
-
-wss.on("connection", (ws) => {
-  ws.on("error", console.error);
-
-  ws.on("message", (data) => {
-    wss.clients.forEach((client) => client.send(data.toString()));
-  });
-});
+const wss = new Service();
+wss.setWs();
+wss.connection();
